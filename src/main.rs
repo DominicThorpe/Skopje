cb6ab4288787df38e91bef50d611b16e7e5c7ff6
Dominic Thorpe <d.thorpe3@lancaster.ac.uk>
2024-12-24 18:23:12 +0000

    Added very basic parsing of simple functions which return int literals.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod lexing;
+mod errors;
+mod parsing;
+
+fn main() {
+    let mut lexer = lexing::Lexer::new("fn main() -> int { 1 }".to_string());
+    lexer.lex().unwrap();
+    
+    let mut parser = parsing::Parser::new(lexer.tokens);
+    let parse_tree = parser.parse().unwrap();
+    
+    println!("{:#?}", parse_tree);
+}
+
+
